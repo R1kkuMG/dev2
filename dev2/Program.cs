66294c3142132e7bba0c1b9bd105d1a7fa6530c0
Maggie <alis-weasley@mail.ru>
2021-12-26 14:18:40 +0300

    Add Array

--- dev2/Program.cs
+++ dev2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из чисел, больших 8. 
+// Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.Clear();
+
+int[] array = new int [50];
+
+Console.Write("Массив {");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 51);
+    Console.Write(" " + array[i] + " ");
+}
+Console.WriteLine("}");
