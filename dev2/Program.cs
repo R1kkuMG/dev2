60e1d76f98cb55423b43f1ca8d05bcf9814f4383
Maggie <alis-weasley@mail.ru>
2021-12-27 18:37:25 +0300

    Update program dev2

--- dev2/Program.cs
+++ dev2/Program.cs
@@ -4,28 +4,56 @@
 
 Console.Clear();
 
-void PrintArray()
+void FillArray(int[] array)
 {
-    int[] array = new int [25];
-    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
         array[i] = new Random().Next(0, 26);
-        Console.Write(" " + array[i] + " ");
     }
-    Console.WriteLine("]");
-
-    Console.WriteLine("->");
-
-    Console.Write("[");
-    for (int j = 0; j < array.Length; j++)
-    {
-        if(array[j] > 8)
-        {
-            Console.Write(" " + array[j] + " ");
-        }
-    }
-    Console.Write("]");
 }
 
-PrintArray();
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int ArraySize(int[] newarray)
+{
+    int newarraylength = 0;
+    for (int i = 0; i < newarray.Length; i++)
+    {
+        if (newarray[i] > 8)
+        {
+            newarraylength++;
+        }
+    }
+    return newarraylength;
+}
+
+void FillNewArray(int[] array, int[] newarray)
+{
+    for (int j = 0, k = 0; j < array.Length; j++)
+    {
+        if (array[j] > 8)
+        {
+            newarray[k] = array[j];
+            k++;
+        }
+    }
+}
+
+void Demo ()
+{
+    int[] array = new int[25];
+    FillArray(array);
+    PrintArray(array);
+    Console.WriteLine("");
+    int[] newarray = new int [ArraySize(array)];
+    FillNewArray (array, newarray);
+    PrintArray(newarray);
+}
+
+Demo ();
